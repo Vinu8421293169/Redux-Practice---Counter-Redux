6d37495d087c4debf8710e443a238d90662a28d8
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-12-27 00:57:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { decCount, incCount } from "../reducers/action";
 
-function App() {
-
+function App(props) {
   return (
-    <div id='main'>
-      
+    <div id="main">
+      <div data-testid="counter">{props.state.count}</div>
+      <button onClick={() => props.dispatch(decCount())}>-</button>
+      <button onClick={() => props.dispatch(incCount())}>+</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  state
+});
+
+export default connect(mapStateToProps)(App);
